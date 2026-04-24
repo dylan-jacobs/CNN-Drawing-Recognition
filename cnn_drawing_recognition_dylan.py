db1d4cfa3ac6fb6ad699d6d00b177e2f6fc36284
djacobs2@swarthmore.edu <djacobs2@swarthmore.edu>
2026-04-24 12:45:29 +0200

    created CNN, trying to train it but getting memory allocation errors

--- cnn_drawing_recognition_dylan.py
+++ cnn_drawing_recognition_dylan.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import random
 from torch.utils.data import random_split
 from google.colab import drive
+import gc
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
@@ -23,9 +24,13 @@ drive.mount('/content/drive')
 
 # %% - Load and Preprocess data
 
-batch_size = 64
+batch_size = 16
+img_size = 64
+
 root = "/content/drive/MyDrive/Colab Notebooks/png_ready"
 transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((img_size, img_size)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5,], std=[0.5,]) # I think images in black and white
 ])
@@ -50,9 +55,9 @@ print(f'Validation length: ', val_len)
 print(f'Testing length: ', test_len)
 
 # Create torch dataloaders
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-val_loader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
-test_loader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, num_workers=2, shuffle=True)
+val_loader = torch.utils.data.DataLoader(valset, batch_size=64, num_workers=2, shuffle=True)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=64, num_workers=2, shuffle=False)
 
 # %% Preview images
 def preview_images(data_loader, classes):
@@ -71,4 +76,85 @@ def preview_images(data_loader, classes):
 classes = dataset.classes
 preview_images(train_loader, classes)
 
-# %% 
+# %% Define CNN class
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, img_size, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(img_size, img_size, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(img_size, img_size, kernel_size=3, padding=1)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(64 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc_out = nn.Linear(256, 250)
+    
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+
+        x = torch.flatten(x, 1) # need to flatten for fully connected layers
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+
+        x = torch.relu(self.fc2(x))
+        x = self.dropout(x)
+
+        x = self.fc_out(x) # no need for relu bc torch does softmax automatically
+        return x
+    
+
+
+# %% Train model
+
+model = CNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1)
+
+def train_model(model, train_loader, val_loader, epochs=10):
+    train_losses, val_losses = [], [] # track for loss curves
+
+    for epoch in range(epochs):
+        total_train_loss = 0.0
+        total_val_loss   = 0.0
+
+        # training step
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            predictions = model(images)
+            loss = criterion(predictions, labels)
+            loss.backward()
+            optimizer.step()
+
+            total_train_loss += loss.item()
+        
+        avg_train_loss = total_train_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+
+        # validation step
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            predictions = model(images)
+            loss = criterion(predictions, labels)
+
+            total_val_loss += loss.item()
+
+        avg_val_loss = total_val_loss / len(val_loader)
+        val_losses.append(avg_val_loss)
+
+        # update LR if necessary according to validation
+        lr_scheduler.step(avg_val_loss)
+
+        print(f'Epoch {epoch}/{epochs}, Train Loss: {avg_train_loss:.4}, Val Loss: {avg_val_loss:.4}, Learning Rate: {optimizer.param_groups[0]['lr']:.6}')
+    return train_losses, val_losses
+
+
+train_losses, val_losses = train_model(model, train_loader, val_loader, epochs=10)
+print('Finished training')
+
+# %%
